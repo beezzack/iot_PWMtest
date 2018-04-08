8806161dde013deddb18e67d4d775fa3f64673bb
beezzack <cliff860429@gmail.com>
2018-04-08 16:10:25 +0800

    hw1 done

--- DHT11/hw1.py
+++ DHT11/hw1.py
@@ -6,34 +6,34 @@ import LED
 sensor = Adafruit_DHT.DHT11
 
 GPIO = 14
-
+theLED = 0
 while True:
-    LED.setup(10, "OUT")
+    LED.Setup(10, "OUT")
     LED.Setup(5, "OUT")
     LED.Setup(13, "OUT")
-
+    currentTime = time.strftime("%H:%M:%S")
 
 
     humidity, temperature = Adafruit_DHT.read_retry(sensor, GPIO)
 
     if temperature is not None:
-        print(currentTim,'-> Temp={0}*C '.format(temperature))
+        print(currentTime,'-> Temp={0}*C '.format(temperature))
 
         if(temperature>=31):
+            if theLED>0:
+                LED.TurnOffLED(theLED)
+            theLED = 13
             LED.TurnOnLED(13)
-            time.sleep(1)
-            LED.TurnOffLED(13)
-            time.sleep(1)
-        else if(temperature>=27 and temperature<=30):
+        elif temperature>=27 and temperature<=30:
+            if theLED>0:
+                LED.TurnOffLED(theLED)
+            theLED = 5
             LED.TurnOnLED(5)
-            time.sleep(1)
-            LED.TurnOffLED(5)
-            time.sleep(1)
-        else if(temperature>=20 and temperature<=26):
-            LED.TurnOnLED(13)
-            time.sleep(1)
-            LED.TurnOffLED(13)
-            time.sleep(1)
+        elif temperature>=20 and temperature<=26:
+            if theLED>0:
+                LED.TurnOffLED(theLED)
+            theLED = 10
+            LED.TurnOnLED(10)
 
 
     else:
