471ed58d0036b0c2a18c08a849a9d61eaf00565b
beezzack <cliff860429@gmail.com>
2018-04-19 21:55:52 +0900

    test2

---
+++ electro/test_2.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+import TurnOnLED
+
+LED.Setup(2, "OUT")
+LED.Setup(3, "OUT")
+
+try:
+    while True:
+        LED.TurnOnLED(2)
+        LED.TurnOffLED(3)
+        time.sleep(1)
+        LED.TurnOffLED(2)
+        LED.TurnOnLED(3)
+        time.sleep(1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
