b41f570e015e1617f88b0278707f4b8d0a135eae
beezzack <cliff860429@gmail.com>
2018-05-16 23:42:22 +0800

    homework exchange

--- bluetooth&wifi/homework_sub.py
+++ bluetooth&wifi/homework_sub.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
         client = mqtt.Client()
         client.on_connect = on_connect
         client.on_message = on_message
-        client.connect("192.168.1.30",1883,60)
+        client.connect("192.168.1.18",1883,60)
         client.loop_forever()
     except KeyboardInterrupt:
         server.close()
