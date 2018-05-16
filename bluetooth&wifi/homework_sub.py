9e4fbc069512881922b650441e9ed0111094e188
beezzack <cliff860429@gmail.com>
2018-05-16 23:02:59 +0800

    type(byt)

--- bluetooth&wifi/homework_sub.py
+++ bluetooth&wifi/homework_sub.py
@@ -12,7 +12,7 @@ def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
     outputstring = msg.topic+" "+str(msg.payload)+"\n\t"
     byt = outputstring.encode()
-    print(byt.type())
+    print(type(byt))
     conn_client.send(byt)
 
 
