63f47f3c7d5bd302da08d28ae35bac86868b73a9
beezzack <cliff860429@gmail.com>
2018-05-31 18:20:49 +0800

    modar

--- motor/hw1.py
+++ motor/hw1.py
@@ -3,45 +3,45 @@ import time
 import LED
 from time import sleep
 
-def setup(GPIOnum):
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(GPIOnum, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+    
 
 
 counter = 0
 def SetAngle(angle):
     dutyCycle = 1/20 * angle + 3
     pwm.ChangeDutyCycle(dutyCycle)
-    sleep(1)
+    sleep(0.5)
 def motion(GPIOnum):
     global counter
 
     if GPIO.input(GPIOnum):
         counter += 1
         LED.TurnOnLED(4)
-
         print("Motion detected{0}".format(counter))
+        pwm.stop()
 
     else:
         LED.TurnOffLED(4)
-        while True:
-            SetAngle(90)
-            SetAngle(180)
-            SetAngle(90)
-            SetAngle(0)
         print("Motion not detected")
+        pwm.start(0)
+
+        
 
 
 if __name__ == "__main__":
     try:
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(3, GPIO.OUT)
-        pwm=GPIO.PWM(3, 50)
+        GPIO.setup(17, GPIO.OUT)
+        pwm=GPIO.PWM(17, 50)
         pwm.start(0)
         LED.Setup(4,"OUT")
-        setup(12)
-        GPIO.add_event_detect(12, GPIO.BOTH, callback = motion, bouncetime = 150)
-
+        GPIO.setup(14, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+        GPIO.add_event_detect(14, GPIO.BOTH, callback = motion, bouncetime = 300)
+        while True:
+            SetAngle(90)
+            SetAngle(180)
+            SetAngle(0)
+            
 
     except KeyboardInterrupt:
         pwm.stop()
