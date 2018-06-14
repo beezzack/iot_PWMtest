f9a25f92669d325fa4630b79530115003a784068
beezzack <cliff860429@gmail.com>
2018-06-14 21:14:23 +0800

    1.0

--- opcv/camera_face_detect.py
+++ opcv/camera_face_detect.py
@@ -1,7 +1,7 @@
 import cv2
 
 cascPath = "haarcascade_frontalface_default.xml"
-faseCascade = cv2.CascadeClassifier(cascPath)
+faceCascade = cv2.CascadeClassifier(cascPath)
 
 cap = cv2.VideoCapture(0)
 
@@ -10,13 +10,13 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = faseCascade.detectMultiScale{
+    faces = faceCascade.detectMultiScale(
         gray,
         scaleFactor=1.1,
         minNeighbors = 5,
         minSize = (60,60),
         flags = cv2.CV_FEATURE_PARAMS_HAAR
-    }
+    )
 
     print("Found {0} faces!".format(len(faces)))
 
--- opcv/webcam-stream.py
+++ opcv/webcam-stream.py
@@ -1,12 +1,12 @@
 import cv2
 
-cpa = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
 while(cap.isOpened()):
     ret, frame = cap.read()
 
     cv2.imshow('Video streaming', frame)
-    if cv2.waitKey(1) & 0xFF = ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 print('Close')
