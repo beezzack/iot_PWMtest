19cd37a4552a15e7005e2dca8edb0e65f2b5336b
beezzack <cliff860429@gmail.com>
2018-06-14 20:23:29 +0800

    opcv

---
+++ opcv/webcam-stream.py
@@ -0,0 +1,14 @@
+import cv2
+
+cpa = cv2.VideoCapture(0)
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+
+    cv2.imshow('Video streaming', frame)
+    if cv2.waitKey(1) & 0xFF = ord('q'):
+        break
+
+print('Close')
+cap.release()
+cv2.destroyAllWindows()
