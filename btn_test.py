322e1949b7e508f9fa9bec667a6bb8dbac510ee5
beezzack <cliff860429@gmail.com>
2018-06-21 01:21:24 +0800

    test

--- btn_test.py
+++ btn_test.py
@@ -7,11 +7,12 @@ btn = 4
 gpio.setup(4,gpio.IN, pull_up_down = gpio.PUD_UP)
 
 try:
+    starttime = time.time()
     while True:
-        time.sleep(0.1)
+        time.sleep(1)
 
-        if(gpio.input(btn) == 0):
-            print("press")
+        #if(gpio.input(btn) == 0):
+        print(str(gpio.input(btn))+", "+str(time.time()-starttime))
 
 except KeyboardInterrupt:
     pass
